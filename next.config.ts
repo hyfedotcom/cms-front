0592bc1cc8c59e76d26a159f175cc6ae0df37886
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2026-01-09 22:35:40 +0100

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -30,27 +30,30 @@ const nextConfig: NextConfig = {
     ],
     minimumCacheTTL: 31536000,
     formats: ["image/avif", "image/webp"],
+    deviceSizes: [360, 430, 768, 1024, 1280, 1440, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 
   async headers() {
     const isProd = process.env.NODE_ENV === "production";
 
+    if (!isProd) {
+      return [
+        {
+          source: "/:path*",
+          headers: [{ key: "Cache-Control", value: "no-store" }],
+        },
+      ];
+    }
     return [
       {
-        source: "/:path*",
-        headers: isProd
-          ? [
-              {
-                key: "Cache-Control",
-                value: "public, max-age=31536000, immutable",
-              },
-            ]
-          : [
-              {
-                key: "Cache-Control",
-                value: "no-store",
-              },
-            ],
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
       },
     ];
   },
